69c530ab05d45e282344ad8cbfa699ed235a831b
Ruslan Kruzhanivskyi <133385255+Atom1c69@users.noreply.github.com>
2023-11-08 13:33:49 +0200

    -

--- src/components/friendList/friendList.jsx
+++
@@ -1,9 +0,0 @@
-import "../../data/friends.json";
- import FriendListItem from "../FriendListItem/FriendListItem"
- import css from "../friendlistitem/friendListItem.module.css";
-
- const FriendList = () => (
-<ul className={css.friend_list}>
-  <FriendListItem/>
-</ul>)
-export default FriendList;
