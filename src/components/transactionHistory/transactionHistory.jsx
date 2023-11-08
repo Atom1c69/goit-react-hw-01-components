b7969613840bfbbcf1dd4ec6b48d74ee1b8ae315
Ruslan Kruzhanivskyi <133385255+Atom1c69@users.noreply.github.com>
2023-11-08 12:38:57 +0200

    update 4.0

--- src/components/transactionHistory/transactionHistory.jsx
+++ src/components/transactionHistory/transactionHistory.jsx
@@ -1,16 +1,32 @@
+import React from "react";
 import transactions from "../../data/transactions.json";
-import TransactionHistoryItem from "../TransactionHistory/TransactionHistoryItem.jsx"
-import css from "../TransactionHistory/TransactionHistory.module.css"
+import css from "../transactionHistory/transactionHistory.module.css"
 
-export const TransactionHistory = () => (<table className={css.transaction_history}>
-<thead>
-  <tr>
-    <th>Type</th>
-    <th>Amount</th>
-    <th>Currency</th>
-  </tr>
-</thead>
-<tbody>
-<TransactionHistoryItem/>
-</tbody>
-</table>);
+const TransactionHistory = () => {
+  const TransactionHistoryItem = () => {
+    return transactions.map(({ id, type, amount, currency }) => (
+      <tr key={id}>
+        <td>{type}</td>
+        <td>{amount}</td>
+        <td>{currency}</td>
+      </tr>
+    ));
+  };
+
+  return (
+    <table className={css.transaction_history}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        <TransactionHistoryItem />
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
